deb561adc4000925ff26b72c698eb8488f942579
firaa <88104941+firaa-ol@users.noreply.github.com>
2022-08-22 20:15:13 +0300

    simple-exercises: divide

--- simple-exercises/src/lib.rs
+++ simple-exercises/src/lib.rs
@@ -31,6 +31,27 @@ fn reverse_integer(number: i32) -> i32{
     }
 }
 
+/*
+ Write a program to divide two integers (dividend and divisor) without using multiplication, division and mod operator
+ */
+fn divide(num1 : i32, num2 : i32) -> i32{
+    let mut count : i32 = 0;
+    let mut first_num = num1.abs();
+    let second_num = num2.abs();
+
+    while first_num >= second_num {
+        count += 1;
+        first_num -= second_num;
+    }
+
+    if (num1 < 0) ^ (num2 < 0) {
+        -1 * count
+    } else {
+        count
+    }
+    
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,5 +61,13 @@ mod tests {
         assert_eq!(reverse_integer(123), 321);
         assert_eq!(reverse_integer(208478933), 339874802);
         assert_eq!(reverse_integer(-73634), -43637);
+    }
+
+    #[test]
+    fn divide_test() {
+        assert_eq!(divide(7, 2), 3);
+        assert_eq!(divide(-17, 5), -3);
+        assert_eq!(divide(35, 7), 5);
+        assert_eq!(divide(-39, -4), 9);
     }
 }
