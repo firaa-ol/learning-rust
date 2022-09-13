fe5efcf8b79afc3e22736ce3721ae265e5e429fe
firaa <88104941+firaa-ol@users.noreply.github.com>
2022-09-13 21:26:38 +0300

    data-structures: make the singly linked list generic

--- data-structures/src/lib.rs
+++ data-structures/src/lib.rs
@@ -1,15 +1,19 @@
-struct SinglyLinkedList {
-    start: Option<Box<Node>>
+//TODO: add reverse method
+
+use std::fmt::Display;
+
+struct SinglyLinkedList<T : Display + Clone> {
+    start: Option<Box<Node<T>>>
 }
 
-impl SinglyLinkedList {
+impl<T : Display + Clone> SinglyLinkedList<T> {
     fn new() -> Self{
         Self {
             start: None
         }
     }
 
-    fn push(&mut self, elt: u32) {
+    fn push(&mut self, elt: T) {
         if self.start.is_none() {
             self.start = Some(Box::new(Node {value: elt, next: None}));
         } else {
@@ -18,11 +22,11 @@ impl SinglyLinkedList {
         }
     }
 
-    fn pop(&mut self) -> Option<u32> {
+    fn pop(&mut self) -> Option<T> {
         if self.start.is_none() {
             None
         } else {
-            let val = self.start.as_ref().unwrap().value;
+            let val = self.start.as_ref().unwrap().value.clone();
             self.start = self.start.as_mut().unwrap().next.take();
             Some(val)
         }
@@ -43,9 +47,9 @@ impl SinglyLinkedList {
     }
 }
 
-struct Node{
-    value: u32,
-    next: Option<Box<Node>>
+struct Node<T : Display + Clone>{
+    value: T,
+    next: Option<Box<Node<T>>>
 }
 
 #[cfg(test)]
@@ -53,7 +57,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn singly_linked_list_integer_works() {
         let mut list = SinglyLinkedList::new();
         list.push(25);
         list.push(46);
@@ -62,5 +66,15 @@ mod tests {
         list.push(544);
         assert!(!list.start.is_none());
         assert_eq!(list.to_string().as_str(), "544,46,25,");
+    }
+
+    #[test]
+    fn singly_linked_list_str_works() {
+        let mut list2 = SinglyLinkedList::new();
+        list2.push("hello");
+        list2.push("goodbye");
+        list2.push("hi");
+
+        assert_eq!(list2.to_string().as_str(), "hi,goodbye,hello,");
     }
 }
