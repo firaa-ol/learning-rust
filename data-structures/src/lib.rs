1e1ece1890eb58a655c3121c6461856583c01918
firaa <88104941+firaa-ol@users.noreply.github.com>
2022-10-04 20:12:00 +0300

    data-structures: add enqueue and dequeue methods to singly linked list

--- data-structures/src/lib.rs
+++ data-structures/src/lib.rs
@@ -3,16 +3,19 @@
 use std::fmt::Display;
 
 struct SinglyLinkedList<T : Display + Clone> {
-    start: Option<Box<Node<T>>>
+    start: Option<Box<Node<T>>>,
+    size: u32
 }
 
 impl<T : Display + Clone> SinglyLinkedList<T> {
     fn new() -> Self{
         Self {
-            start: None
+            start: None,
+            size: 0
         }
     }
 
+    // push to the front of the list
     fn push(&mut self, elt: T) {
         if self.start.is_none() {
             self.start = Some(Box::new(Node {value: elt, next: None}));
@@ -20,15 +23,71 @@ impl<T : Display + Clone> SinglyLinkedList<T> {
             let new_node = Some(Box::new(Node {value: elt, next: self.start.take() }));
             self.start = new_node;
         }
+        self.size += 1;
     }
 
+    // pop from the front of the list
     fn pop(&mut self) -> Option<T> {
         if self.start.is_none() {
             None
         } else {
             let val = self.start.as_ref().unwrap().value.clone();
             self.start = self.start.as_mut().unwrap().next.take();
+            self.size -= 1;
             Some(val)
+        }
+    }
+
+    // add at the end of the list
+    fn enqueue(&mut self, elt: T) {
+        let last_node = self.get_last_element();
+        if last_node.is_none() {
+            self.start = Some(Box::new(Node {value: elt, next: None}));
+        } else {
+            last_node.unwrap().next = Some(Box::new(Node {value: elt, next: None}));
+        }
+        self.size += 1;
+    }
+
+    // remove from end of the list
+    fn dequeue(&mut self) -> Option<T> {
+        if self.start.is_none() {
+            None
+        } else {
+            self.size -= 1;
+            let before_last_node = self.get_before_last_element();          
+            Some(before_last_node.unwrap().next.take().unwrap().value)
+        }
+    }
+
+    fn get_last_element(&mut self) -> Option<&mut Box<Node<T>>> {
+        if self.start.is_none() {
+            None
+        } else {
+            let mut node = self.start.as_mut().unwrap();
+            while !node.next.is_none() {
+                node = node.next.as_mut().unwrap();
+            }
+
+            Some(node)
+        }
+    }
+
+    //get the element before the last element
+    fn get_before_last_element(&mut self) -> Option<&mut Box<Node<T>>> {
+        if self.start.is_none() {
+            None
+        } else {
+            let mut node = self.start.as_mut().unwrap();
+            while !node.next.is_none() {
+                if node.next.as_ref().unwrap().next.is_none(){
+                    break;
+                }
+
+                node = node.next.as_mut().unwrap();
+            }
+
+            Some(node)
         }
     }
 
@@ -103,6 +162,23 @@ mod tests {
         let reversed_list = list.rev();
         assert_eq!(list.to_string().as_str(), "3,2,1,");
         assert_eq!(reversed_list.to_string().as_str(), "1,2,3,");
+    }
+
+    #[test]
+    fn singly_linked_list_queue_works() {
+        let mut list = SinglyLinkedList::new();
+        list.enqueue(10);
+        list.enqueue(23);
+        list.enqueue(345);
+        list.enqueue(77);
+
+        assert_eq!(list.size, 4);
+        assert_eq!(list.to_string(), "10,23,345,77,");
+
+        list.dequeue();
+
+        assert_eq!(list.size, 3);
+        assert_eq!(list.to_string(), "10,23,345,");
     }
 
 }
