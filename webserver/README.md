d9779486ee87534244ed9929e271448976721ff0
firaa <88104941+firaa-ol@users.noreply.github.com>
2022-10-10 21:59:40 +0300

    webserver: add a simple multi threaded web server

---
+++ webserver/README.md
@@ -0,0 +1,14 @@
+## Simple MultiThreaded WebServer
+
+Applied Concepts
+- Tcp Listener, Tcp Stream, Buffer Reader
+- Threads
+- Multiple producer single consumer channels
+- Atomic Refrence Counter and Mutex
+- Closures
+- Drop Trait
+
+Next Steps
+- [] Better Error Handling
+- [] Serve Content from a Directory
+- [] Handle HTTPS requests
---
+++ webserver/src/main.rs
@@ -0,0 +1,121 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream },
+    thread,
+    sync::{mpsc, Arc, Mutex},
+    time::Duration
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender: Some(sender) }
+    }
+
+    pub fn execute<F>(&self, f: F)
+     where F : FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread : Option<thread::JoinHandle<()>>
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+
+            match receiver.lock().unwrap().recv() {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                },
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker { id, thread : Some(thread) }
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting dowm");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request : Vec<_> = buf_reader.lines()
+                                .map(|res| res.unwrap())
+                                .take_while(|line| !line.is_empty())
+                                .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let request_line = &http_request[0];
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };    
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();  
+}
